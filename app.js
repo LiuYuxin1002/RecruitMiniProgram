6f29d131ff972683b31511255549b825e6c1d06b
liuyuxinzju <21725152@zju.edu.cn>
2018-09-05 00:10:54 +0800

    修改my页面

--- app.js
+++ app.js
@@ -1,61 +1,39 @@
-var WxParse = require('wxParse/wxParse.js');
-var util = require('utils/util.js');
-//var request = require('utils/request.js');
+//app.js
 App({
   onLaunch: function () {
-  },
-  onShareAppMessage: function () {
-    return {
-      success: function (res) {
-        // 分享成功
-        console.log(res);
-      },
-      fail: function (res) {
-        // 分享失败
-        console.log(res);
-      }
-    }
-  },
-  getUserInfo: function () {
-    var that = this;
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
     wx.login({
-      success: function (res) {
-      console.log(res);
-        if (res.code) {
-          //发起网络请求
-          // request.login(
-          //   {
-          //     "code": res.code
-          //   }, (res) => {
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
 
-          //     console.log(res);
-          //     var session_id = res.data.session_id;
-          //     that.globalData.session_id = res.data.session_id;
-          //     wx.getUserInfo({
-          //       success: function (res) {
-          //         console.log(11111);
-          //         console.log(res);
-
-          //         request.checkLogin(
-          //           {
-          //             'iv': res.iv,
-          //             'encryptedData': res.encryptedData,
-          //             'session_id': session_id
-          //           }, (res) => {
-          //             console.log(res);
-          //           }
-          //         )
-          //       }
-          //     })
-          //   }
-          // )
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
         }
       }
-    });
+    })
   },
   globalData: {
+    userInfo: null
   }
 })
-
